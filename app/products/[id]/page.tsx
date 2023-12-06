b387cb7771c164b0b2e4029deb64b72ac142639d
lbsudo <lbsudo100@gmail.com>
2023-12-06 12:13:11 -0800

    structure updates

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -43,26 +43,9 @@ export default async function Page({ params }: pageProps) {
 
 
 export async function generateStaticParams() {
-  // Fetch the list of product ids from your API
-  // const API_KEY = process.env.NEXT_PUBLIC_PRINTFUL_API_KEY;
-  // const API_URL = 'https://api.printful.com';
-  // const url = `${API_URL}/store/products`;
-  // const res = await fetch(url, {
-  //   method: 'GET',
-  //   headers: {
-  //     Authorization: `Bearer ${API_KEY}`,
-  //     'Content-Type': 'application/json',
-  //     'Access-Control-Allow-Origin': '*',
-  //   },
-  // });
-  // const data = await res.json();
-  // const products: Product[] = data.result;
-  // const products = await getProducts();
 
   return await getIds();
-  // Return an array of params, one for each product
-  // return products.map(product => ({ id: product.id.toString() }));
-  // return products.map(product => ({ id: product.id.toString() }));
+
 }
 
 
